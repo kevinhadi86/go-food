84cf79412d673ca59242a600c94998641b758fab
kevinhadi86 <kevinhadinata86@gmail.com>
2018-09-24 19:31:24 +0700

    add Factory girl to rails_helper.rb & change some food_spec.rb for easier input

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -2,36 +2,29 @@ require 'rails_helper'
 
 describe Food do
     it "has a valid factory" do
-        expect(FactoryGirl.build(:food)).to be_valid
+        expect(build(:food)).to be_valid
     end
+    
     it "is valid with a name and description" do
-        expect(FactoryGirl.build(:food)).to be_valid
+        expect(build(:food)).to be_valid
     end
     
     it "is invalid without a name" do
-        food = FactoryGirl.build(:food, name: nil)
+        food = build(:food, name: nil)
         food.valid?
         expect(food.errors[:name]).to include("can't be blank")
     end
-    
+
     it "is invalid without a description" do
-        food = FactoryGirl.build(:food, description: nil)
+        food = build(:food, description: nil)
         food.valid?
         expect(food.errors[:description]).to include("can't be blank")
     end
+    
     it "is invalid with a duplicate name" do
-        food1 = Food.create(
-        name: "Nasi Uduk",
-        description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-        price: 10000.0
-        )
-        
-        food2 = Food.new(
-        name: "Nasi Uduk",
-        description: "Just with a different description.",
-        price: 10000.0
-        )
-
+        food1 = create(:food, name: "Nasi Uduk")
+        food2 = build(:food, name: "Nasi Uduk")
+    
         food2.valid?
         expect(food2.errors[:name]).to include("has already been taken")
     end
