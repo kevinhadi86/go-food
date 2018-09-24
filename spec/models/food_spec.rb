3da06c9edc38cc2844928f90bd05a3f77981602b
kevinhadi86 <kevinhadinata86@gmail.com>
2018-09-24 19:29:04 +0700

    add factories and change some rspec with factories data

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,29 +1,21 @@
 require 'rails_helper'
 
 describe Food do
-    it "is valid with a name and description" do
-        food = Food.new(
-        name: "Nasi Uduk",
-        description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-        price: 10000.0
-        )
-        expect(food).to be_valid
+    it "has a valid factory" do
+        expect(FactoryGirl.build(:food)).to be_valid
     end
+    it "is valid with a name and description" do
+        expect(FactoryGirl.build(:food)).to be_valid
+    end
+    
     it "is invalid without a name" do
-        food = Food.new(
-        name: nil,
-        description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-        price: 10000.0
-        )
+        food = FactoryGirl.build(:food, name: nil)
         food.valid?
         expect(food.errors[:name]).to include("can't be blank")
     end
+    
     it "is invalid without a description" do
-        food = Food.new(
-        name: "Nasi Uduk",
-        description: nil,
-        price: 10000.0
-        )
+        food = FactoryGirl.build(:food, description: nil)
         food.valid?
         expect(food.errors[:description]).to include("can't be blank")
     end
